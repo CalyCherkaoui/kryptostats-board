6e8e5774f0f2f0adbbf498b3d5627e92b2e2711b
Houda Cherkaoui <houda.cherkaoui@gmail.com>
2021-07-10 15:26:30 -0500

    Add Pages and components to App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
 import './App.css';
+import Navigation from './components/Navigation';
+import CoinsIndexPage from './pages/CoinsIndexPage';
+import AboutPage from './pages/AboutPage';
+import CoinDetailPage from './pages/CoinDetailPage';
 
 function App() {
   return (
     <div className="App">
-      Kryptostats
+      <BrowserRouter>
+        <Navigation />
+        <Route exact path="/" component={CoinsIndexPage} />
+        <Route exact path="/about" component={AboutPage} />
+        <Route exact path="/item" component={CoinDetailPage} />
+      </BrowserRouter>
     </div>
   );
 }
