626566f272624d97bac9e370be8e4f4b56d8cc10
Houda Cherkaoui <houda.cherkaoui@gmail.com>
2021-07-10 15:20:13 -0500

    Add navigation component

---
+++ src/components/Navigation.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Navigation() {
+  return (
+    <div>
+      <h1 className="nav_logo">Krypto-stats | Dashboard</h1>
+      <ul>
+        <li className="nav_item">About</li>
+        <li className="nav_item">Index</li>
+        <li className="nav_item">Item</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Navigation;
