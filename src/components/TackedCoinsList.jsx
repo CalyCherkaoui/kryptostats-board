4dd20db416df6a6b46494851477f7aee3605f55a
Houda Cherkaoui <houda.cherkaoui@gmail.com>
2021-07-10 16:39:08 -0500

    Implement connection to coinGecko api

--- src/components/TackedCoinsList.jsx
+++ src/components/TackedCoinsList.jsx
@@ -1,7 +1,27 @@
+/* eslint-disable no-console */
 /* eslint-disable arrow-body-style */
-import React from 'react';
+// eslint-disable-next-line no-unused-vars
+import React, { useEffect, useState } from 'react';
+import coinGecko from '../apis/coinGecko';
 
 const TackedCoinsList = () => {
+  // const [coins, setCoins] = useState([]);
+  useEffect(
+    () => {
+      const fetchData = async () => {
+        const response = await coinGecko.get('coins/markets', {
+          params: {
+            vs_currency: 'usd',
+            ids: 'bitcoin,ethereum',
+          },
+        });
+
+        console.log(response.data);
+      };
+      fetchData();
+    },
+    [], // fetch the data once
+  );
   return (
     <div>
       list of tracked coins
