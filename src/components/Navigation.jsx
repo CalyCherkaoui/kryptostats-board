ef98e7af920d2a3fb4f92a86479fc75adc6602cb
Houda Cherkaoui <houda.cherkaoui@gmail.com>
2021-07-10 15:31:16 -0500

    Implement links in navigation component

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Navigation() {
   return (
     <div>
       <h1 className="nav_logo">Krypto-stats | Dashboard</h1>
       <ul>
-        <li className="nav_item">About</li>
-        <li className="nav_item">Index</li>
-        <li className="nav_item">Item</li>
+        <li className="nav_item">
+          <Link to="/about" className="nav_link">About</Link>
+        </li>
+        <li className="nav_item">
+          <Link to="/" className="nav_link">Index</Link>
+        </li>
+        <li className="nav_item">
+          <Link to="/item" className="nav_link">Item</Link>
+        </li>
       </ul>
     </div>
   );
