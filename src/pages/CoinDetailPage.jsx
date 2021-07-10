ad5c32ab5706c959365f179f422a123e6d9ed0e5
Houda Cherkaoui <houda.cherkaoui@gmail.com>
2021-07-10 15:16:41 -0500

    Add Coin detail page

---
+++ src/pages/CoinDetailPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+// eslint-disable-next-line arrow-body-style
+const CoinDetailPage = () => {
+  return (
+    <div>
+      Coin Details
+    </div>
+  );
+};
+
+export default CoinDetailPage;
