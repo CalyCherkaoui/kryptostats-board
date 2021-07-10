0b9f9d1668354862e95d6b224130d613702a4f31
Houda Cherkaoui <houda.cherkaoui@gmail.com>
2021-07-10 15:58:24 -0500

    Add coin to track add btn component

---
+++ src/components/AddCoinToTrack.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable arrow-body-style */
+import React from 'react';
+
+const AddCoinToTrack = () => {
+  return (
+    <div>
+      <button type="button">Add coin to track</button>
+    </div>
+  );
+};
+
+export default AddCoinToTrack;
