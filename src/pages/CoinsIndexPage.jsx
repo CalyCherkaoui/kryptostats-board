69f1af4acc50dcd85e2a89cff53f106e5d8e56db
Houda Cherkaoui <houda.cherkaoui@gmail.com>
2021-07-10 15:15:29 -0500

    Add Coins index page

---
+++ src/pages/CoinsIndexPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+// eslint-disable-next-line arrow-body-style
+const CoinsIndexPage = () => {
+  return (
+    <div>
+      Coins index page
+    </div>
+  );
+};
+
+export default CoinsIndexPage;
