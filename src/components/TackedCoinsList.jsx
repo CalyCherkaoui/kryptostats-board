2b988366f057c4f0f98cbbb0d81912df49b6aed5
Houda Cherkaoui <houda.cherkaoui@gmail.com>
2021-07-10 16:00:05 -0500

    Add Tracked coins list components

---
+++ src/components/TackedCoinsList.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable arrow-body-style */
+import React from 'react';
+
+const TackedCoinsList = () => {
+  return (
+    <div>
+      list of tracked coins
+    </div>
+  );
+};
+
+export default TackedCoinsList;
