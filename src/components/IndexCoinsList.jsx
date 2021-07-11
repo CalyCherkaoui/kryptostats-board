460c5ad167d148c47660b05ec4d7c5475d2c6f6e
Houda Cherkaoui <houda.cherkaoui@gmail.com>
2021-07-11 12:42:24 -0500

    Implement rendreing method for the index page

--- src/components/IndexCoinsList.jsx
+++ src/components/IndexCoinsList.jsx
@@ -1,11 +1,15 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable no-console */
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import coinGecko from '../apis/coinGecko';
 
 const IndexCoinsList = () => {
+  const [coins, setCoins] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
   useEffect(
     () => {
       const fetchData = async () => {
+        setIsLoading(true);
         const response = await coinGecko.get('coins/markets', {
           params: {
             vs_currency: 'eur',
@@ -14,16 +18,31 @@ const IndexCoinsList = () => {
             price_change_percentage: '24h',
           },
         });
-
         console.log(response.data);
+        setCoins(response.data);
+        setIsLoading(false);
+        console.log(isLoading);
       };
       fetchData();
     },
     [],
   );
+  const renderCoins = () => {
+    if (isLoading) {
+      return <div>Loading data</div>;
+    }
+    return (
+      <ul className="coins_list">
+        {coins.map(
+          (coin) => <Coin key={coin.id} coin={coin} />,
+        )}
+      </ul>
+    );
+  };
   return (
     <div>
-      index list
+      index
+      <div>{renderCoins()}</div>
     </div>
   );
 };
