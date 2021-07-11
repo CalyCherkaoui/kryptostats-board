b8747b46dc46f1a33f9e5bc5649f161d858028ed
Houda Cherkaoui <houda.cherkaoui@gmail.com>
2021-07-11 14:37:33 -0500

    Implement icons rendering methods

--- src/components/Coin.jsx
+++ src/components/Coin.jsx
@@ -2,12 +2,27 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { IconContext } from 'react-icons';
-import { FaAngleUp } from 'react-icons/fa';
+import { FaAngleUp, FaAngleDown } from 'react-icons/fa';
 import { GrAddCircle } from 'react-icons/gr';
 import { AiOutlineCloseCircle } from 'react-icons/ai';
 
 const Coin = ({ coin, indexList }) => {
-  const iconRender = () => {
+  const upDownIconRender = () => {
+    if (coin.price_change_percentage_24h >= 0) {
+      return (
+        <IconContext.Provider value={{ color: 'green', className: 'up_icon' }}>
+          <FaAngleUp />
+        </IconContext.Provider>
+      );
+    }
+    return (
+      <IconContext.Provider value={{ color: 'red', className: 'down_icon' }}>
+        <FaAngleDown />
+      </IconContext.Provider>
+    );
+  };
+
+  const btnIconRender = () => {
     if (indexList) {
       return (
         <IconContext.Provider value={{ color: 'green', className: 'add_icon' }}>
@@ -24,16 +39,16 @@ const Coin = ({ coin, indexList }) => {
   return (
     <Link to="/coininfo">
       <li className="coinlist_item">
-        <img src={coin.image} alt={coin.id} className="coinlist_item_image" />
+        <img src={coin.image} alt={coin.name} className="coinlist_item_image" />
         <span className="coinlist_item_price">{coin.current_price}</span>
         <span className="coinlist_item_change">
-          <IconContext.Provider value={{ color: 'green', className: 'priceup_icon' }}>
-            <FaAngleUp />
-          </IconContext.Provider>
-          {coin.price_percentage_24}
+          <i className="upDown_icon">
+            { upDownIconRender()}
+          </i>
+          {coin.price_change_percentage_24h}
         </span>
         <button className="coinlist_item_btn" type="button">
-          { iconRender() }
+          { btnIconRender() }
         </button>
       </li>
     </Link>
@@ -43,10 +58,10 @@ const Coin = ({ coin, indexList }) => {
 Coin.propTypes = {
   coin: PropTypes.shape(
     {
-      id: PropTypes.string,
+      name: PropTypes.string,
       image: PropTypes.string,
       current_price: PropTypes.number,
-      price_percentage_24: PropTypes.number,
+      price_change_percentage_24h: PropTypes.number,
     },
   ).isRequired,
   indexList: PropTypes.bool.isRequired,
