6e4304425ef2ad2802f71e061d127b5dd3b61ede
Houda Cherkaoui <houda.cherkaoui@gmail.com>
2021-07-12 16:30:15 -0500

    Implement Redux Coins actions

---
+++ src/redux/actions/CoinsActions.js
@@ -0,0 +1,37 @@
+// import axios from "axios";
+import coinGecko from '../../apis/coinGecko';
+
+export const GetCoinsIndexList = (
+  page,
+  localCurrency,
+  priceChangePercentage,
+) => async (dispatch) => {
+  try {
+    dispatch({ type: 'COINS_LIST_LOADING' });
+    const perPage = 6;
+    // const offset = (page * perPage) - page;
+    const responseCoins = await coinGecko.get('coins/markets', {
+      params: {
+        vs_currency: localCurrency,
+        per_page: perPage,
+        page,
+        price_change_percentage: priceChangePercentage,
+      },
+    });
+
+    dispatch({
+      type: 'COINS_LIST_SUCCESS',
+      payload: responseCoins.data,
+    });
+  } catch (e) {
+    dispatch({ type: 'COINS_LIST_FAIL' });
+  }
+};
+
+export const GetCoinInfo = () => async (dispatch) => {
+  try {
+    dispatch({ type: 'COIN_INFO_LOADING' });
+  } catch (e) {
+    dispatch({ type: 'COIN_INFO_FAIL' });
+  }
+};
