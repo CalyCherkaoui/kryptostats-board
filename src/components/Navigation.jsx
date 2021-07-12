e3d2c4545a4fe194087dab5ac59436f516dea26a
Houda Cherkaoui <houda.cherkaoui@gmail.com>
2021-07-12 10:56:29 -0500

    Add filter component to Navigation

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,21 +1,29 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import LocalCurrencyFilter from './LocalCurrencyFilter';
 
 function Navigation() {
   return (
-    <div>
-      <h1 className="nav_logo">Krypto-stats | Dashboard</h1>
-      <ul>
-        <li className="nav_item">
-          <Link to="/about" className="nav_link">About</Link>
-        </li>
-        <li className="nav_item">
-          <Link to="/" className="nav_link">Index</Link>
-        </li>
-        <li className="nav_item">
-          <Link to="/tracked" className="nav_link">Tracked Items</Link>
-        </li>
-      </ul>
+    <div className="navigation_bar">
+      <div className="navigation_left">
+        <div>
+          <h1 className="nav_logo">Krypto-stats | Dashboard</h1>
+          <ul>
+            <li className="nav_item">
+              <Link to="/about" className="nav_link">About</Link>
+            </li>
+            <li className="nav_item">
+              <Link to="/" className="nav_link">Index</Link>
+            </li>
+            <li className="nav_item">
+              <Link to="/tracked" className="nav_link">Tracked Items</Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div className="navigation_right">
+        <LocalCurrencyFilter />
+      </div>
     </div>
   );
 }
