2ddf56750430e0e3ba3dc9c71b5c673bdfc89db1
Houda Cherkaoui <houda.cherkaoui@gmail.com>
2021-07-14 17:28:56 -0500

    Implement Coin Info actions

--- src/redux/actions/CoinsActions.js
+++ src/redux/actions/CoinsActions.js
@@ -1,5 +1,4 @@
-// import axios from "axios";
-import coinGecko from '../../apis/coinGecko';
+import axios from 'axios';
 
 export const GetCoinsIndexList = (
   pageNum,
@@ -9,16 +8,8 @@ export const GetCoinsIndexList = (
   try {
     dispatch({ type: 'COINS_LIST_LOADING' });
     const perPage = 6;
-    // const offset = (page * perPage) - page;
-    const responseCoins = await coinGecko.get('coins/markets', {
-      params: {
-        vs_currency: localCurrency,
-        per_page: perPage,
-        page: pageNum,
-        price_change_percentage: priceChangePercentage,
-      },
-    });
-
+    const Url = `https://api.coingecko.com/api/v3/coins/markets?vs_currency=${localCurrency}&order=market_cap_desc&per_page=${perPage}&page=${pageNum}&sparkline=false&price_change_percentage=${priceChangePercentage}`;
+    const responseCoins = await axios.get(Url);
     dispatch({
       type: 'COINS_LIST_SUCCESS',
       payload: responseCoins.data,
@@ -28,9 +19,16 @@ export const GetCoinsIndexList = (
   }
 };
 
-export const GetCoinInfo = () => async (dispatch) => {
+export const GetCoinInfo = (coinid) => async (dispatch) => {
   try {
     dispatch({ type: 'COIN_INFO_LOADING' });
+    const Url = `https://api.coingecko.com/api/v3/coins/${coinid}`;
+    const responseCoin = await axios.get(Url);
+    dispatch({
+      type: 'COIN_INFO_SUCCESS',
+      payload: responseCoin.data,
+      coinId: coinid,
+    });
   } catch (e) {
     dispatch({ type: 'COIN_INFO_FAIL' });
   }
