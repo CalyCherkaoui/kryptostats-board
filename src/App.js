0af1fa586795786f90bf8c2389ceab54425464c1
Houda Cherkaoui <houda.cherkaoui@gmail.com>
2021-07-14 17:28:06 -0500

    Update routing

--- src/App.js
+++ src/App.js
@@ -2,26 +2,20 @@ import React from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import './App.css';
 import Navigation from './components/Navigation';
-// import CoinsIndexPage from './pages/CoinsIndexPage';
 import AboutPage from './pages/AboutPage';
 import CoinDetailPage from './pages/CoinDetailPage';
 import CoinsList from './components/CoinsList';
-import TrackedCoinsPage from './pages/TrackedCoinsPage';
-import { TrackedCoinsListContextProvider } from './context/TrackedCoinsContext';
 
 function App() {
   return (
     <div className="App">
-      <TrackedCoinsListContextProvider>
-        <Navigation />
-        <Switch>
-          <Route exact path="/" component={CoinsList} />
-          <Route exact path="/about" component={AboutPage} />
-          <Route exact path="/tracked" component={TrackedCoinsPage} />
-          <Route path="/coininfo/:coinid" component={CoinDetailPage} />
-          <Redirect to="/" />
-        </Switch>
-      </TrackedCoinsListContextProvider>
+      <Navigation />
+      <Switch>
+        <Route exact path="/" component={CoinsList} />
+        <Route exact path="/about" component={AboutPage} />
+        <Route exact path="/coininfo/:coinid" component={CoinDetailPage} />
+        <Redirect to="/" />
+      </Switch>
     </div>
   );
 }
