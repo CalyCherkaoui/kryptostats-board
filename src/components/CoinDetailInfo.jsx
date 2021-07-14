c7bd7d42ca90e26f998023b62d32c71554da3801
Houda Cherkaoui <houda.cherkaoui@gmail.com>
2021-07-14 17:27:46 -0500

    Refactor Coin detail info coponent

--- src/components/CoinDetailInfo.jsx
+++ src/components/CoinDetailInfo.jsx
@@ -1,52 +1,24 @@
-/* eslint-disable arrow-body-style */
 /* eslint-disable no-console */
-import PropTypes from 'prop-types';
-import parse from 'html-react-parser';
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useParams } from 'react-router-dom';
+import { GetCoinInfo } from '../redux/actions/CoinsActions';
 
-const CoinDetailInfo = ({
-  infoImage,
-  infoName,
-  infoSymbol,
-  infoDescription,
-  infoCurrentPrice,
-  infoChange24,
-  infoVolume,
-}) => {
-  const description = `<p>${infoDescription}</p>`;
-  const parsedDescription = parse(description);
+const CoinDetailInfo = () => {
+  const { coinid } = useParams();
+  const dispatch = useDispatch();
+  const coinInfoState = useSelector((state) => state.CoinInfo);
+  React.useEffect(() => {
+    dispatch(GetCoinInfo(coinid));
+    console.log(coinid);
+    console.log(coinInfoState);
+  }, []);
+
   return (
-    <div className="coin_page_info_wrapper">
-      <div className="coin_page_about">
-        <img src={infoImage} alt={infoName} className="coin_page_about_image" />
-        <div className="coin_page_name_symbol">
-          <span>{infoName}</span>
-          <span>{infoSymbol}</span>
-        </div>
-        <div className="coin_page_about_description">{parsedDescription}</div>
-        <div className="coin_page_data">
-          <div className="coin_page_data_current_price">
-            {infoCurrentPrice}
-          </div>
-          <div className="coin_page_data_total_vol">
-            {infoVolume}
-          </div>
-          <div className="coin_page_data_change24h">
-            {infoChange24}
-          </div>
-        </div>
-      </div>
+    <div>
+      coin
     </div>
   );
-};
-
-CoinDetailInfo.propTypes = {
-  infoName: PropTypes.string.isRequired,
-  infoSymbol: PropTypes.string.isRequired,
-  infoImage: PropTypes.string.isRequired,
-  infoDescription: PropTypes.string.isRequired,
-  infoCurrentPrice: PropTypes.number.isRequired,
-  infoChange24: PropTypes.number.isRequired,
-  infoVolume: PropTypes.number.isRequired,
 };
 
 export default CoinDetailInfo;
