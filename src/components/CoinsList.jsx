331206dee576a6b57fa3a2d51ee7b8d4c76c129c
Houda Cherkaoui <houda.cherkaoui@gmail.com>
2021-07-15 15:51:40 -0500

    Implement fetchDataPaginator for filtred list

--- src/components/CoinsList.jsx
+++ src/components/CoinsList.jsx
@@ -1,14 +1,19 @@
+/* eslint-disable no-console */
 /* eslint-disable no-unused-vars */
-import React from 'react';
+import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import _ from 'lodash';
 import ReactPaginate from 'react-paginate';
 import { GetCoinsIndexList } from '../redux/actions/CoinsActions';
+import { GetWithLocalCurrencyFilter } from '../redux/actions/FiltresActions';
 import Coin from './Coin';
+import LocalCurrencyFilter from './LocalCurrencyFilter';
 
 const CoinsList = () => {
   const dispatch = useDispatch();
+  const [localCurrency, setLocalCurrency] = useState('usd');
   const coinsList = useSelector((state) => state.CoinsList);
+  const filteredCoinsByLocalCurrency = useSelector((state) => state.FilteredCoinsByLocalCurrency);
 
   const fetchData = (page = 1) => {
     dispatch(GetCoinsIndexList(
@@ -16,36 +21,70 @@ const CoinsList = () => {
     ));
   };
 
+  const fetchDataPaginator = (page = 1) => {
+    if (filteredCoinsByLocalCurrency.listUpdated) {
+      dispatch(GetWithLocalCurrencyFilter(
+        page, localCurrency, '24h',
+      ));
+    } else {
+      dispatch(GetCoinsIndexList(
+        page, 'usd', '24h',
+      ));
+    }
+  };
+
+  const changeLocalCurrencyHandler = (currency) => {
+    dispatch(GetWithLocalCurrencyFilter(
+      1, currency, '24h',
+    ));
+    setLocalCurrency(currency);
+    console.log(currency);
+    console.log('filtred handler');
+    console.log(filteredCoinsByLocalCurrency.data);
+  };
+
   React.useEffect(() => {
     fetchData(1);
-    // eslint-disable-next-line no-console
+    console.log('coinslist');
     console.log(coinsList);
-  }, []);
+    console.log('filtred useeffect');
+    console.log(filteredCoinsByLocalCurrency.data);
+  }, [filteredCoinsByLocalCurrency]);
 
-  const displayData = () => {
-    if (coinsList.loading) {
+  const displayData = (myCoinsList) => {
+    if (myCoinsList.loading) {
       return <p>loading..</p>;
     }
 
-    if (!_.isEmpty(coinsList.data)) {
-      return coinsList.data.map(
+    if (!_.isEmpty(myCoinsList.data)) {
+      return myCoinsList.data.map(
         (coin) => (
           <Coin coin={coin} key={`key_${coin.id}`} />
         ),
       );
     }
 
-    if (coinsList.errorMessage !== '') {
+    if (myCoinsList.errorMessage !== '') {
       return <p>error</p>;
     }
 
     return <p>default</p>;
   };
 
+  const showList = () => {
+    if (filteredCoinsByLocalCurrency.listUpdated) {
+      return displayData(filteredCoinsByLocalCurrency);
+    }
+    return displayData(coinsList);
+  };
+
   return (
     <div className="coins_list_container">
+      <div className="coin_list_filters">
+        <LocalCurrencyFilter changeLocalCurrency={changeLocalCurrencyHandler} />
+      </div>
       <div className="coins_list">
-        {displayData()}
+        { showList() }
       </div>
       <div className="pagination_wrapper">
         {!_.isEmpty(coinsList.data)
@@ -55,7 +94,7 @@ const CoinsList = () => {
           pageCount={100}
           pageRangeDisplayed={1}
           marginPagesDisplayed={2}
-          onPageChange={(data) => fetchData(data.selected + 1)}
+          onPageChange={(data) => fetchDataPaginator(data.selected + 1)}
           containerClassName="pagination_container"
         />
         )}
