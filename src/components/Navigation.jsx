cefdce9cf1556a156afc7f548f865860d55b1b08
Houda Cherkaoui <houda.cherkaoui@gmail.com>
2021-07-15 17:43:15 -0500

    Style navigation bar

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -2,24 +2,26 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import SearchBar from './SearchBar';
+import styles from '../styles/Navigation.module.css';
 
 function Navigation() {
   return (
-    <div className="navigation_bar">
-      <div className="navigation_left">
-        <div>
-          <h1 className="nav_logo">Krypto-stats | Dashboard</h1>
-          <ul>
-            <li className="nav_item">
-              <Link to="/about" className="nav_link">About</Link>
-            </li>
-            <li className="nav_item">
-              <Link to="/" className="nav_link">Index</Link>
-            </li>
-          </ul>
-        </div>
+    <div className={`${styles.navigation_bar} flex_row flex_space_between`}>
+      <div className={`${styles.navigation_left} flex_row flex_start_bl`}>
+        <p className={`${styles.nav_logo} bold_typography`}>
+          Krypto|
+          <span className={styles.nav_logo_emph}>stats</span>
+        </p>
+        <ul className="flex_row flex_start_bl">
+          <li className={`${styles.nav_item} normal_typography`}>
+            <Link to="/about" className="nav_link">About</Link>
+          </li>
+          <li className={`${styles.nav_item} normal_typography`}>
+            <Link to="/" className="nav_link">Index</Link>
+          </li>
+        </ul>
       </div>
-      <div className="navigation_right">
+      <div className={styles.navigation_right}>
         <SearchBar />
       </div>
     </div>
