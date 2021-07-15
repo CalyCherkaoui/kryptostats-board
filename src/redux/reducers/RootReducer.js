7c491ea82590f94c43fdd1a84ed0dd83df747dce
Houda Cherkaoui <houda.cherkaoui@gmail.com>
2021-07-15 15:52:47 -0500

    Update root reducer

--- src/redux/reducers/RootReducer.js
+++ src/redux/reducers/RootReducer.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
+import LocalCurrencyFilterReducer from './LocalCurrencyFilterReducer';
 import CoinInfoReducer from './CoinInfoReducer';
 import CoinsListReducer from './CoinsListReducer';
 
 const RootReducer = combineReducers({
   CoinsList: CoinsListReducer,
   CoinInfo: CoinInfoReducer,
+  FilteredCoinsByLocalCurrency: LocalCurrencyFilterReducer,
 });
 
 export default RootReducer;
