0480a11f34ea70bb72c7e00897e3c1976edd68f7
Houda Cherkaoui <houda.cherkaoui@gmail.com>
2021-07-19 16:52:21 -0500

    Implement App tests

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,6 +1,9 @@
 import { shallow } from 'enzyme';
-// import renderer from 'react-test-renderer';
+import renderer from 'react-test-renderer';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import App from '../App';
+import store from '../redux/store';
 
 describe(
   'Renders correctely App component',
@@ -14,6 +17,41 @@ describe(
       'Contains Navigation Bar',
       () => {
         expect(container.find('Navigation')).toBeTruthy();
+      },
+    );
+
+    it(
+      'Contains CoinsList component',
+      () => {
+        expect(container.find('CoinsList')).toBeTruthy();
+      },
+    );
+
+    it(
+      'Contains AboutPage component',
+      () => {
+        expect(container.find('AboutPage')).toBeTruthy();
+      },
+    );
+
+    it(
+      'Contains CoinDetailPage component',
+      () => {
+        expect(container.find('CoinDetailPage')).toBeTruthy();
+      },
+    );
+
+    it(
+      'Matches App snapshot',
+      () => {
+        const rendered = renderer.create(
+          <Provider store={store}>
+            <Router>
+              <App />
+            </Router>
+          </Provider>,
+        );
+        expect(rendered).toMatchSnapshot();
       },
     );
   },
