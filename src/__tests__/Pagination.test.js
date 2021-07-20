24d3de0c729c59f8cdf61467d87ea3cdfb77f1cf
Houda Cherkaoui <houda.cherkaoui@gmail.com>
2021-07-20 09:17:35 -0500

    Implement Pagination tests

--- src/__tests__/Pagination.test.js
+++ src/__tests__/Pagination.test.js
@@ -1,29 +1,23 @@
+// import { shallow } from 'enzyme';
 import renderer from 'react-test-renderer';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from '../redux/store';
-import Coin from '../components/Coin';
+import Pagination from '../components/Pagination';
 
 describe(
   'Renders correctly The navigation bar component',
   () => {
-    let coin;
-    beforeEach(() => {
-      coin = {
-        id: 'testcoin',
-        image: 'image_link',
-        current_price: 1,
-        price_change_percentage_24h: 1,
-      };
-    });
+    const count = 1;
+    const fetchData = () => true;
 
     it(
-      'Matches Coin snapshot',
+      'Matches Pagination snapshot',
       () => {
         const rendered = renderer.create(
           <Provider store={store}>
             <Router>
-              <Coin coin={coin} />
+              <Pagination count={count} fetchData={fetchData} />
             </Router>
           </Provider>,
         );
