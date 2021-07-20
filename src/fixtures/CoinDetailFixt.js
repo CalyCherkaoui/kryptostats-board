36125179f0bae8a775728be4fba56b7c528e6d3a
Houda Cherkaoui <houda.cherkaoui@gmail.com>
2021-07-20 14:05:25 -0500

    Add CoinDetail fixture for testing

---
+++ src/fixtures/CoinDetailFixt.js
@@ -0,0 +1,13 @@
+const CoinDetailDataFixt = {
+  name: 'bitcoin',
+  image: { large: 'https://assets.coingecko.com/coins/images/1/large/bitcoin.png?1547033579' },
+  Symbol: 'btc',
+  links: ['http://www.bitcoin.org'],
+  market_data: {
+    current_price: { usd: 'USD', eur: 'EUR' },
+    price_change_percentage_24h: 1,
+  },
+  description: { en: 'English test description' },
+};
+
+export default CoinDetailDataFixt;
