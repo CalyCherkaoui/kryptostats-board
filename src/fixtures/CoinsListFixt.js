73ab6bc4779b5922c2bd8e24b7e33fe5c20e94d8
Houda Cherkaoui <houda.cherkaoui@gmail.com>
2021-07-20 14:05:10 -0500

    Add CoinsList fixture for testing

---
+++ src/fixtures/CoinsListFixt.js
@@ -0,0 +1,60 @@
+const CoinsListFixt = [
+  {
+    id: 'bitcoin',
+    name: 'bitcoin',
+    image: 'https://assets.coingecko.com/coins/images/1/large/bitcoin.png?1547033579',
+    current_price: 1,
+    price_change_percentage_24h: 2,
+  },
+  {
+    id: 'bitcoin2',
+    name: 'bitcoin2',
+    image: 'https://assets.coingecko.com/coins/images/1/large/bitcoin.png?1547033579',
+    current_price: 1,
+    price_change_percentage_24h: 2,
+  },
+  {
+    id: 'bitcoin3',
+    name: 'bitcoin3',
+    image: 'https://assets.coingecko.com/coins/images/1/large/bitcoin.png?1547033579',
+    current_price: 1,
+    price_change_percentage_24h: 2,
+  },
+  {
+    id: 'bitcoin4',
+    name: 'bitcoin4',
+    image: 'https://assets.coingecko.com/coins/images/1/large/bitcoin.png?1547033579',
+    current_price: 1,
+    price_change_percentage_24h: 2,
+  },
+  {
+    id: 'bitcoin5',
+    name: 'bitcoin5',
+    image: 'https://assets.coingecko.com/coins/images/1/large/bitcoin.png?1547033579',
+    current_price: 1,
+    price_change_percentage_24h: 2,
+  },
+  {
+    id: 'bitcoin6',
+    name: 'bitcoin6',
+    image: 'https://assets.coingecko.com/coins/images/1/large/bitcoin.png?1547033579',
+    current_price: 1,
+    price_change_percentage_24h: 2,
+  },
+  {
+    id: 'bitcoin7',
+    name: 'bitcoin7',
+    image: 'https://assets.coingecko.com/coins/images/1/large/bitcoin.png?1547033579',
+    current_price: 1,
+    price_change_percentage_24h: 2,
+  },
+  {
+    id: 'bitcoin8',
+    name: 'bitcoin8',
+    image: 'https://assets.coingecko.com/coins/images/1/large/bitcoin.png?1547033579',
+    current_price: 1,
+    price_change_percentage_24h: 2,
+  },
+];
+
+export default CoinsListFixt;
