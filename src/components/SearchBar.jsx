12db8135efcc5d66465040acde96cdea43eba831
Houda Cherkaoui <houda.cherkaoui@gmail.com>
2021-07-20 11:35:46 -0500

    Fixing the Proptypes issue in SearchBar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,6 +1,6 @@
-/* eslint-disable react/prop-types */
 import React, { useState } from 'react';
 import { withRouter } from 'react-router-dom';
+import PropTypes from 'prop-types';
 import styles from '../styles/SearchBar.module.css';
 
 const SearchBar = (props) => {
@@ -12,6 +12,10 @@ const SearchBar = (props) => {
       <button className={styles.search_bar_button} type="button" onClick={() => props.history.push(`/coininfo/${search}`)}>Search</button>
     </div>
   );
+};
+
+SearchBar.propTypes = {
+  history: PropTypes.instanceOf(Object).isRequired,
 };
 
 export default withRouter(SearchBar);
